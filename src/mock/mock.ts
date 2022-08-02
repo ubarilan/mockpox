5867b93839894a83b559c41ef2be3b15438a7d75
Uri Bar Ilan <ubarilan@gmail.com>
2022-08-03 01:22:20 +0300

    Added SSL support for mock mode

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -8,6 +8,7 @@ import ReadConfig from '../lib/readconfig';
 import { MockConf } from '../lib/types/mockTypes';
 import { logger } from '../lib/utils/logger';
 import handler from './handler';
+import { createCertificate } from 'pem';
 
 export default class Mock {
     private server: httpServer | httpsServer; // Implement HTTPS
@@ -26,24 +27,45 @@ export default class Mock {
         this.initServer();
     }
 
-    private initServer() {
-        if (!this.conf.ssl) {
-            this.server = createHttpServer();
-        } else {
-            this.server = createHttpsServer();
-            throw new Error('Not implemented');
-        }
-
+    private startWebServer() {
         try {
             this.server.listen(this.conf.port, this.conf.address, () => {
                 this.logger.info(
-                    `Listening on ${this.conf.address}:${this.conf.port}`
+                    `Mock server listening on ${this.conf.address}:${this.conf.port}`
                 );
             });
-        } catch (error) {
-            throw new Error(`An error occured: ${error}`);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                this.logger.crit(
+                    `Error when trying to listen on port ${this.conf.port}: ${err.message}`
+                );
+            }
+            process.exit(1);
         }
-
         this.server.on('request', this.handler.bind(this));
+    }
+
+    private initServer() {
+        if (!this.conf.ssl) {
+            this.server = createHttpServer();
+            this.startWebServer();
+        } else {
+            createCertificate(
+                {
+                    days: 1,
+                    selfSigned: true,
+                },
+                (err, keys) => {
+                    if (err) {
+                        throw err;
+                    }
+                    this.server = createHttpsServer({
+                        key: keys.serviceKey,
+                        cert: keys.certificate,
+                    });
+                    this.startWebServer();
+                }
+            );
+        }
     }
 }
